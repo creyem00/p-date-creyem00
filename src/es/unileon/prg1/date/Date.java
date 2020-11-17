aa648ecf717fd84caf95d7e33c1f581411f30f2f
Carlos <creyem00@estudiante.unileon.es>
2020-11-17 18:24:22 +0100

    Added PracticaDate

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -59,9 +59,103 @@ public class Date {
 		
 		return numDays;
 	}
+	private void getNameOfMonth() {
 	
-	public String toString() {
-		return this.day + "/" + this.month + "/" + this.year;
+		
+		switch (this.month) {
+		case 1: //next
+				System.out.println("Enero");
+				break;
+		case 2: //next
+				System.out.println("Febrero");
+				break;
+		case 3: //next
+				System.out.println("Marzo");
+				break;
+		case 4: //next
+				System.out.println("Abril");
+				break;
+		case 5: //next
+				System.out.println("Mayo");
+				break;
+		case 6: //next
+				System.out.println("Junio");
+				break;
+		case 7: //next
+				System.out.println("Julio");
+				break;
+		case 8: //next
+				System.out.println("Agosto");
+				break;
+		case 9: //next
+				System.out.println("Septiembre");
+				break;
+		case 10: //next
+				 System.out.println("Octubre");
+				 break;
+		case 11: //next
+				 System.out.println("Noviembre");
+			     break;
+		case 12: //next
+				 System.out.println("Diciembre");
+				 break;	
+						
+		}
+		
+	}
+	
+	public int getDay() {
+	return day;
+	}
+	public int getMonth() {
+	return month;
+	}
+	public int getYear() {
+	return year;
 	}
 
+	public boolean isSameYear(Date miDate){
+		boolean esIgual = false;
+		if (this.year == miDate.getYear()){
+			esIgual = true;
+		}
+		return esIgual;
+	}	
+	public boolean isSameYearWiththoutif(Date miDate){
+		
+		return(this.year == miDate.getYear());
+			
+	}
+
+	public boolean isSameMonth(Date miDate){
+		boolean esIgual = false;
+		if(this.month == miDate.getMonth()){
+			esIgual = true;
+		}
+		return esIgual;
+	}	
+	public boolean isSameMonthwithoutif(Date miDate){
+
+		return(this.month == miDate.getMonth());
+
+	}
+
+	public boolean isSameDay(Date miDate){
+		boolean esIgual = false;
+		if(this.day == miDate.getDay()){
+			esIgual = true;
+		}
+	    return esIgual;
+	}
+	public boolean isSameDaywithoutif(Date miDate){
+		return(this.day == miDate.getDay());
+	}
+
+
+
+
+	public String toString() {
+		getNameOfMonth();
+		return this.day + "/" + this.month + "/" + this.year;
+	}
 }
--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -3,10 +3,18 @@ package es.unileon.prg1.date;
 public class MainDate {
 
 	public static void main(String[] args) {
-		Date today;
+		Date today, tomorrow;
 		
 		try {
 			today = new Date(11,11,2020);
+			tomorrow = new Date(17,11,2020);
+
+			if(today.isSameYear(tomorrow)){
+				System.out.println("Son el mismo year");
+			}else{
+				System.out.println("Son years distintos");
+			}
+
 			System.out.println(today);
 		} catch (DateException e) {
 			System.out.println(e.getMessage());
